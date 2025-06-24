5bd11f2bbf7513a3b464c7dde190ac4291915cee
tletranvn <tletranvn@gmail.com>
2025-06-24 11:16:26 +0200

    Valide Docker 8081 & localhost:8000

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default_index')]
+    public function index()
+    {
+        return new Response(
+            '<html><body>Welcome to the default controller!</body></html>'
+        );
+    }
+}
