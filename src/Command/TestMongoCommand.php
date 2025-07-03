52298a27e3b2bd0ece2422a9f2693ee9a94c1796
tletranvn <tletranvn@gmail.com>
2025-07-03 13:37:52 +0200

    MongoDB - VichUploader OK

--- src/Command/TestMongoCommand.php
+++ src/Command/TestMongoCommand.php
@@ -1,34 +1,39 @@
 <?php
 namespace App\Command;
 
-use App\Document\Photo;
-use Doctrine\ODM\MongoDB\DocumentManager;
-use Symfony\Component\Console\Attribute\AsCommand;
+use MongoDB\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'app:test-mongo')]
 class TestMongoCommand extends Command
 {
-    public function __construct(private DocumentManager $dm)
+    private $mongoClient;
+
+    public function __construct(Client $mongoClient)
     {
         parent::__construct();
+        $this->mongoClient = $mongoClient;
+    }
+
+    protected function configure()
+    {
+        $this->setName('app:test-mongo');
+        $this->setDescription('Test connection to MongoDB');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $photo = new Photo();
-        $photo->setFilename('test.jpg');
-        $photo->setOriginalFilename('original.jpg');
-        $photo->setMimeType('image/jpeg');
-        $photo->setSize(123456);
-        $photo->setRelatedArticleId('123');
-
-        $this->dm->persist($photo);
-        $this->dm->flush();
-
-        $output->writeln('Photo insérée avec succès');
-        return Command::SUCCESS;
+        try {
+            $databases = $this->mongoClient->listDatabases();
+            $output->writeln('MongoDB connection OK. Databases found:');
+            foreach ($databases as $db) {
+                $output->writeln(' - ' . $db->getName());
+            }
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $output->writeln('MongoDB connection failed: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
     }
 }
