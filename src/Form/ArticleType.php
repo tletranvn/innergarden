37092be9cdc7a34cfc7d983b686cbbe96397e9cb
tletranvn <tletranvn@gmail.com>
2025-07-28 13:46:33 +0200

    Fix CSRF for heroku login and form submission (security.yaml & ArticleType.php)

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -110,6 +110,7 @@ class ArticleType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+            'csrf_protection' => false, // Disable CSRF for Heroku compatibility
         ]);
     }
 }
